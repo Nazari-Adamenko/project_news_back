5a2232c731f407ec31c843d69ab631ae5da77bc2
Nazari <n.adamenko@dunice.net>
2022-07-01 17:50:37 +0300

    last commit 01.07.22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,22 @@ class UsersController < ApplicationController
     render json: @user.to_json(include: :posts)
   end
 
+  # PATCH/ /user/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :avatar)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   resources :posts
 
   devise_for :users,
-            controllers: {
-                sessions: 'users/sessions',
-                registrations: 'users/registrations'
-            }
+          controllers: {
+            sessions: 'users/sessions',
+            registrations: 'users/registrations'
+          }
   get '/member-data', to: 'members#show'
   get '/users/:id', to: 'users#show'
+  patch '/user/:id', to: 'user#update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
