c4fa79934d0a013a49f1d5cf59030bcab849b71a
Nazari <n.adamenko@dunice.net>
2022-07-06 15:02:56 +0300

    fix code review techlid only metods

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :users
+  resources :users, only: [:show, :update]
+  resources :members, only: [:show]
   devise_for :users,
           controllers: {
             sessions: 'users/sessions',
             registrations: 'users/registrations'
           }
-  get '/member-data', to: 'members#show'
-  # get '/users/:id', to: 'users#show'
-  # patch '/users/:id', to: 'users#update'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
